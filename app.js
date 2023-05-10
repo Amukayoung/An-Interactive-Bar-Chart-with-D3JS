73a6fd47e6eb91c594a627a6123cbd98e4962a61
@Amukayoung <amukayoung@gmail.com>
2023-05-11 00:52:40 +0300

    Added mocked Uganda cities population

---
+++ app.js
@@ -0,0 +1,42 @@
+Mocked_Data = [
+	{
+		id: "001",
+		cityName: "Gulu",
+		population: "146,000",
+	},
+	{
+		id: "002",
+		cityName: "Lira",
+		population: "119,000",
+	},
+	{
+		id: "003",
+		cityName: "Mbarara",
+		population: "97,000",
+	},
+	{
+		id: "004",
+		cityName: "Mbale",
+		population: "76,000",
+	},
+	{
+		id: "005",
+		cityName: "Soroti",
+		population: "56,000",
+	},
+	{
+		id: "006",
+		cityName: "Fort Portal",
+		population: "42,000",
+	},
+	{
+		id: "007",
+		cityName: "Masaka",
+		population: "65,000",
+	},
+	{
+		id: "008",
+		cityName: "Jinja",
+		population: "93,000",
+	},
+];
