b54273ec36e2cd6dd32d5b06eef8b6b1daaf0fd0
@Amukayoung <amukayoung@gmail.com>
2023-05-11 01:20:05 +0300

    Added x and y axis scales

--- app.js
+++ app.js
@@ -2,52 +2,58 @@ Mocked_Data = [
 	{
 		id: "001",
 		cityName: "Gulu",
-		population: "146,000",
+		population: 146000,
 	},
 	{
 		id: "002",
 		cityName: "Lira",
-		population: "119,000",
+		population: 119000,
 	},
 	{
 		id: "003",
 		cityName: "Mbarara",
-		population: "97,000",
+		population: 97000,
 	},
 	{
 		id: "004",
 		cityName: "Mbale",
-		population: "76,000",
+		population: 76000,
 	},
 	{
 		id: "005",
 		cityName: "Soroti",
-		population: "56,000",
+		population: 56000,
 	},
 	{
 		id: "006",
 		cityName: "Fort Portal",
-		population: "42,000",
+		population: 42000,
 	},
 	{
 		id: "007",
 		cityName: "Masaka",
-		population: "65,000",
+		population: 65000,
 	},
 	{
 		id: "008",
 		cityName: "Jinja",
-		population: "93,000",
+		population: 93000,
 	},
 ];
 
 const ChartWidth = 750;
 const ChartHeight = 450;
 
+const xAxisScale = d3.scaleBand().rangeRound([0, ChartWidth]).padding(0.2);
+const yAxisScale = d3.scaleLinear.range([ChartHeight, 0]);
+
 const chartContainer = d3
 	.select("svg")
 	.attr("width", ChartWidth)
 	.attr("height", ChartHeight);
+
+xAxisScale.domain(Mocked_Data.map((data) => data.cityName));
+yAxisScale.domain([0, d3.max(Mocked_Data, (data) => data.population) + 10000]);
 
 const chart = chartContainer
 	.append("g")
